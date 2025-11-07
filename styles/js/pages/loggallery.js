da580ee62c8c89fbc34a7de6f9f9bc97da9cc776
M4CH1N4097 <jabberwocks134@gmail.com>
2025-11-08 04:37:42 +0900

    Update loggallery.js

--- styles/js/pages/loggallery.js
+++ styles/js/pages/loggallery.js
@@ -8,16 +8,17 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-
   let dex = await charadex.initialize.page(
     null,
     charadex.page.loggallery,
     null, 
     async (listData) => {
-
+      // 현재 프로필 데이터 저장
+      if (listData?.profileArray?.[0]) {
+        window.charadexCurrentData = listData.profileArray[0];
+      }
     }
   );
-  
+
   charadex.tools.loadPage('.softload', 500);
-  
 });
